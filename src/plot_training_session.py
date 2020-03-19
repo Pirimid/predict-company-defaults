5cda766567d164cfb900ea3cd73e6fe71ef648c3
urvish <urvishp80@gmail.com>
2020-03-19 12:09:57 +0530

    added plotting function

---
+++ src/plot_training_session.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+
+
+def plot_session(train_loss_results, train_accuracy_results):
+    """
+     Plots the training results to visualize the training accuracy and training loss.
+      Arguments:
+       * train_loss_results- Training loss results
+       * train_accuracy_results- Training accuracy 
+    """
+    fig, axes = plt.subplots(2, sharex=True, figsize=(12, 8))
+    fig.suptitle('Training Metrics')
+
+    axes[0].set_ylabel("Loss", fontsize=14)
+    axes[0].plot(train_loss_results)
+
+    axes[1].set_ylabel("Accuracy", fontsize=14)
+    axes[1].set_xlabel("Epoch", fontsize=14)
+    axes[1].plot(train_accuracy_results)
+    plt.show()
