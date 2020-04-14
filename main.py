8a8158b3633d7fbe92b5adef4ae100bb521826ce
urvish <urvishp80@gmail.com>
2020-04-14 17:10:17 +0530

    Adding ta-lib indicators as features, updated training loop as per new features.

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from tensorflow.keras.models import load_model
 import pandas as pd
 from tqdm import tqdm
 import time
-from sklearn.preprocessing import MinMaxScaler, StandardScaler, RobustScaler, QuantileTransformer, PowerTransformer 
-from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, RobustScaler, QuantileTransformer, PowerTransformer
+from sklearn.metrics import accuracy_score, f1_score
 from sklearn.model_selection import StratifiedKFold
 
 from src.generator import DataGenerator
@@ -16,6 +16,7 @@ from src.loss import grad
 from src.date_map import get_next_day
 from src.trainer import train
 from src.plot_training_session import plot_session
+from src.indicators import enrich_data
 
 LOGGER = setup_logger()  # set up the logger.
 scaler = StandardScaler()
@@ -27,28 +28,30 @@ config = {'model_name': 'BiLSTM',
           'mode': 'train_test',
           'optimizer': tf.keras.optimizers.SGD(learning_rate=0.01),
           'base_name': 'sgd_lr_0.001',
-          'time_window':5,
-          'epochs':1
+          'time_window': 5,
+          'epochs': 1
           }
 
 # main train function.
+
+
 def trainer(model,
-            model_name, 
+            model_name,
             data_file,
             train_data,
-            valid_data, 
-            optimizer, 
-            epochs=2, 
+            valid_data,
+            optimizer,
+            epochs=2,
             time_window=5,
-            batch_size=32, 
-            test_size=0.2, 
-            save_model=False, 
+            batch_size=32,
+            test_size=0.2,
+            save_model=False,
             mode='train_test',
             plot_his=False,
             base_name='baseline'):
     """
      Main train function. 
-     
+
       Arguments:-
       * model_name:- Name of the model from config
       * data_file:- Name of the npy data file to laod
@@ -67,52 +70,56 @@ def trainer(model,
     LOGGER.info("Loading the training data...")
     data = np.load(f'data/{data_file}.npy')
     data = data.item()
-    
+
     LOGGER.info(f"Training with time window of {time_window}...")
-    
+
     final_train_loss_his, final_train_acc_his = [], []
-    
+
     # keys
     # keys = list(train_data.keys())
-    # np.random.shuffle(keys) 
-    
+    # np.random.shuffle(keys)
+
     # test_index = int(len(keys) * test_size)
     # train_keys = keys[:len(keys) - test_index]
     # test_keys = keys[len(keys) - test_index:]
-    
+
     LOGGER.info(f"Got {len(train_data)} companies for training...")
     LOGGER.info(f"Got {len(valid_data)} companies for testing...")
-    
+
     if mode == 'train' or mode == 'train_test':
         for key in tqdm(train_data['Keys']):
-            
+
             d = data[key]
             dataframe = d[0]
-            solvency_date = d[1]
-            final_timeStep = d[2] * len(dataframe)  # multiply by len to create tensors of equal length for training.
-            score = d[3] * len(dataframe)           # multiply by len to create tensors of equal length for training.
+            solvency_date = d[1] # multiply by len to create tensors of equal length for training.
+            final_timeStep = d[2] * len(dataframe) # multiply by len to create tensors of equal length for training.
+            score = d[3] * len(dataframe)
             LOGGER.info(f"Total length of training data is {len(dataframe)}")
             LOGGER.info(f"Score : {d[3]}")
-            
+
             dataframe = dataframe.drop('Date', axis=1)
-            columns = dataframe.columns  # to use in creating float from strings.
-            
+            # to use in creating float from strings.
+            columns = dataframe.columns
+
             # TODO: remove this from the main loop to outside.
             def convert_string_to_float(x):
                 for col in columns:
                     x[col] = float(str(x[col]).replace(",", "").replace(
                         ' -   ', str(0)).replace("%", ""))
                 return x
-            
+
             # apply the convert_string_to_float to get the float from string.
             dataframe = dataframe.apply(convert_string_to_float, axis=1)
             dataframe = dataframe.astype(np.float32)
+            dataframe = dataframe.drop('index', axis=1)
+            dataframe = enrich_data(dataframe)
             normalized_data = scaler.fit_transform(dataframe.values)
             lstm_data = prepare_lstm_data(normalized_data, time_window)
-            
+
             LOGGER.info("Starting training...")
-            model.fit(lstm_data, np.array(score[:-time_window]), epochs=epochs, batch_size=32, verbose=2)
-        
+            model.fit(lstm_data, np.array(
+                score[:-time_window]), epochs=epochs, batch_size=32, verbose=2)
+
     if save_model and (mode == 'train_test' or mode == 'train'):
         LOGGER.info("Saving the model now...")
         model.save(f'data/{model_name}_{base_name}.h5')
@@ -120,45 +127,52 @@ def trainer(model,
     if mode == 'test':
         LOGGER.info(f"Loading model {model_name}_{base_name}...")
         model = load_model(f'data/{model_name}_{base_name}.h5')
-    
+
     if mode == 'test' or mode == 'train_test':
         LOGGER.info("Running in testing mode now...")
         true_labels = []
-        predictions  = []
-        
+        predictions = []
+
         for key in tqdm(valid_data['Keys']):
-            
+
             d = data[key]
             dataframe = d[0]
             solvency_date = d[1]
-            final_timeStep = d[2] * len(dataframe)  # multiply by len to create tensors of equal length for training.
-            score = d[3] * len(dataframe)           # multiply by len to create tensors of equal length for training.
+            # multiply by len to create tensors of equal length for training.
+            final_timeStep = d[2] * len(dataframe)
+            # multiply by len to create tensors of equal length for training.
+            score = d[3] * len(dataframe)
             LOGGER.info(f"Total length of testing data is {len(dataframe)}")
             LOGGER.info(f"Score : {d[3]}")
-            
+
             dataframe = dataframe.drop('Date', axis=1)
-            columns = dataframe.columns  # to use in creating float from strings.
-            
+            # to use in creating float from strings.
+            columns = dataframe.columns
+
             # TODO: remove this from the main loop to outside.
             def convert_string_to_float(x):
                 for col in columns:
                     x[col] = float(str(x[col]).replace(",", "").replace(
                         ' -   ', str(0)).replace("%", ""))
                 return x
-            
+
             # apply the convert_string_to_float to get the float from string.
             dataframe = dataframe.apply(convert_string_to_float, axis=1)
             dataframe = dataframe.astype(np.float32)
+            dataframe = dataframe.drop('index', axis=1)
+            dataframe = enrich_data(dataframe)
             normalized_data = scaler.fit_transform(dataframe.values)
-            
+
             lstm_data = prepare_lstm_data(normalized_data, time_window)
 
             preds = model.predict(lstm_data, batch_size=32)
-            LOGGER.info(f"Accuracy : {accuracy_score(np.array(score[:-time_window]).reshape(-1,1), preds.round())}")    
-            true_labels.extend(np.array(score[:-time_window]).reshape(-1,1))
+            LOGGER.info(
+                f"Accuracy : {accuracy_score(np.array(score[:-time_window]).reshape(-1,1), preds.round())}")
+            true_labels.extend(np.array(score[:-time_window]).reshape(-1, 1))
             predictions.extend(preds.round())
-            
-        LOGGER.info(f"Overall accuracy on testing set : {accuracy_score(true_labels, predictions)}")
+
+        LOGGER.info(
+            f"Overall accuracy on testing set : {accuracy_score(true_labels, predictions)}")
 
     if plot_his:
         plot_session(final_train_loss_his, final_train_acc_his)
@@ -167,31 +181,33 @@ def trainer(model,
 if __name__ == "__main__":
     # Read dataframe with keys and score values.
     df = pd.read_csv('data/train_v2.csv')
-    
+
     skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
-    
+
     LOGGER.info("Initializing the model")
     model_init = MODEL_DISPATCHER[config['model_name']]
-    model = model_init(input_shape=(config['time_window'],93), output_shape=(1))
+    model = model_init(input_shape=(
+        config['time_window'], 326), output_shape=(1))
     model = model.create_model()
-    
+
     # compile the model
-    model.compile(optimizer=config['optimizer'], loss='binary_crossentropy', metrics=['accuracy'])
-    
+    model.compile(optimizer=config['optimizer'],
+                  loss='binary_crossentropy', metrics=['accuracy'])
+
     for train_idx, valid_idx in skf.split(X=df['Keys'], y=df['Scores']):
         train_df = df.loc[train_idx]
         valid_df = df.loc[valid_idx]
-        
-        trainer(model, 
-                config['model_name'], 
+
+        trainer(model,
+                config['model_name'],
                 data_file=config['data_file'],
-                train_data=train_df, 
+                train_data=train_df,
                 valid_data=valid_df,
-                optimizer=config['optimizer'], 
+                optimizer=config['optimizer'],
                 mode=config['mode'],
                 base_name=config['base_name'],
                 time_window=config['time_window'],
                 epochs=config['epochs']
                 )
-    
-    model.save('data/biLstm_5_folds_std_scaler.h5')
+
+    # model.save('data/biLstm_5_folds_std_scaler.h5')
---
+++ src/indicators.py
@@ -0,0 +1,142 @@
+import talib
+import pandas as pd
+
+from src.logger import LOGGER
+
+def enrich_data(data):
+    """
+    Enhances data frame with information on indicators and price patterns. Indicators and patterns are align so
+    that they represent details for previous minute.
+
+    :param data: DataFrame
+    :return: DataFrame
+    """
+    LOGGER.info("Adding TA-Lib indicators as features...")
+    # We specifically do shifting here so that all additional data represents information about past history.
+    return pd.concat((data, get_indicators(data).shift(), get_price_patterns(data).shift()), axis=1)
+
+
+def get_indicators(data, intervals=(5, 10, 20, 50, 100)):
+    """
+    Computes technical indicators given ticks data.
+    These indicators are computed with fixed parameters, i.e. intervals argument shouldn't affect them:
+    * Parabolic SAR
+    * Chaikin A/D Line
+    * On Balance Volume
+    * Hilbert Transform - Instantaneous Trendline
+    * Hilbert Transform - Trend vs Cycle Mode
+    * Hilbert Transform - Dominant Cycle Period
+    * Hilbert Transform - Dominant Cycle Phase
+    * Typical Price
+    These indicators are computed for each of periods given in intervals argument:
+    * Exponential Moving Average
+    * Double Exponential Moving Average
+    * Kaufman Adaptive Moving Average
+    * Midpoint Price over period
+    * Triple Exponential Moving Average
+    * Average Directional Movement Index
+    * Aroon
+    * Commodity Channel Index
+    * Momentum
+    * Rate of change Percentage: (price-prevPrice)/prevPrice
+    * Relative Strength Index
+    * Ultimate Oscillator (based on T, 2T, 3T periods)
+    * Williams' %R
+    * Normalized Average True Range
+    * Time Series Forecast (linear regression)
+    * Bollinger Bands
+    For more details see TA-lib documentation.
+    When there are options in indicator API, Close Price prices are used for computation. For volume TickVol is used.
+    Note that some of the indicators are not stable and could output unexpected results if fed with NaNs or long series.
+
+    :param trader DataFrame with data.
+    :param intervals Iterable with time periods to use for computation.
+                     Periods should be in the same sample units as ticks data, i.e. in minutes.
+                     Default values: 5, 10, 20, 50 and 100 minutes.
+    :return DataFrame with indicators. For interval-based indicators, interval is mentioned in column name, e.g. CCI_5.
+    """
+    indicators = {}
+    # Time period based indicators.
+    for i in intervals:
+        indicators['DEMA_{}'.format(i)] = talib.DEMA(
+            data['Close Price'], timeperiod=i)
+        indicators['EMA_{}'.format(i)] = talib.EMA(data['Close Price'], timeperiod=i)
+        indicators['KAMA_{}'.format(i)] = talib.KAMA(
+            data['Close Price'], timeperiod=i)
+        indicators['MIDPRICE_{}'.format(i)] = talib.MIDPRICE(
+            data['High Price'], data['Low Price'], timeperiod=i)
+        indicators['T3_{}'.format(i)] = talib.T3(data['Close Price'], timeperiod=i)
+        indicators['ADX_{}'.format(i)] = talib.ADX(
+            data['High Price'], data['Low Price'], data['Close Price'], timeperiod=i)
+        indicators['AROON_down_{}'.format(i)], indicators['AROON_up_{}'.format(i)] = talib.AROON(
+            data['High Price'], data['Low Price'], timeperiod=i)
+        indicators['CCI_{}'.format(i)] = talib.CCI(
+            data['High Price'], data['Low Price'], data['Close Price'], timeperiod=i)
+        indicators['MOM_{}'.format(i)] = talib.MOM(data['Close Price'], timeperiod=i)
+        indicators['ROCP_{}'.format(i)] = talib.ROCP(
+            data['Close Price'], timeperiod=i)
+        indicators['RSI_{}'.format(i)] = talib.RSI(data['Close Price'], timeperiod=i)
+        indicators['ULTOSC_{}'.format(i)] = talib.ULTOSC(data['High Price'], data['Low Price'], data['Close Price'],
+                                                         timeperiod1=i, timeperiod2=2 * i, timeperiod3=4 * i)
+        indicators['WILLR_{}'.format(i)] = talib.WILLR(
+            data['High Price'], data['Low Price'], data['Close Price'], timeperiod=i)
+        indicators['NATR_{}'.format(i)] = talib.NATR(
+            data['High Price'], data['Low Price'], data['Close Price'], timeperiod=i)
+        indicators['TSF_{}'.format(i)] = talib.TSF(data['Close Price'], timeperiod=i)
+        indicators['BBANDS_upper_{}'.format(i)], indicators['BBANDS_middle_{}'.format(i)], indicators['BBANDS_Low Priceer_{}'.format(i)] = talib.BBANDS(
+            data['Close Price'], timeperiod=5, nbdevup=2, nbdevdn=2, matype=0)
+        indicators['ATR_{}'.format(i)] = talib.ATR(
+            data['High Price'], data['Low Price'], data['Close Price'], timeperiod=i)
+        indicators['NATR_{}'.format(i)] = talib.NATR(
+            data['High Price'], data['Low Price'], data['Close Price'], timeperiod=i)
+        indicators['BETA_{}'.format(i)] = talib.BETA(
+            data['High Price'], data['Low Price'], timeperiod=i)
+        indicators['CORREL_{}'.format(i)] = talib.CORREL(
+            data['High Price'], data['Low Price'], timeperiod=1)
+        indicators['LINEARREG_ANGLE_{}'.format(i)] = talib.LINEARREG_ANGLE(
+            data['Close Price'], timeperiod=i)
+        indicators['Close Price_STDDEV_{}'.format(i)] = talib.STDDEV(
+            data['Close Price'], timeperiod=i)
+        indicators['High Price_STDDEV_{}'.format(i)] = talib.STDDEV(
+            data['High Price'], timeperiod=i)
+        indicators['Low Price_STDDEV_{}'.format(i)] = talib.STDDEV(
+            data['Low Price'], timeperiod=i)
+        indicators['Open Price_STDDEV_{}'.format(i)] = talib.STDDEV(
+            data['Open Price'], timeperiod=i)
+        indicators['Close Price_VAR_{}'.format(i)] = talib.VAR(
+            data['Close Price'], timeperiod=i, nbdev=1)
+        indicators['Open Price_VAR_{}'.format(i)] = talib.VAR(
+            data['Open Price'], timeperiod=i, nbdev=1)
+        indicators['High Price_VAR_{}'.format(i)] = talib.VAR(
+            data['High Price'], timeperiod=i, nbdev=1)
+        indicators['Low Price_VAR_{}'.format(i)] = talib.VAR(
+            data['Low Price'], timeperiod=i, nbdev=1)
+    # Indicators that do not depend on time periods.
+    indicators['Close Price_macd'], indicators['Close Price_macdsignal'], indicators['Close Price_macdhist'] = talib.MACD(
+        data['Close Price'], fastperiod=12, slowperiod=26, signalperiod=9)
+    indicators['Open Price_macd'], indicators['Open Price_macdsignal'], indicators['Open Price_macdhist'] = talib.MACD(
+        data['Open Price'], fastperiod=12, slowperiod=26, signalperiod=9)
+    indicators['High Price_macd'], indicators['High Price_macdsignal'], indicators['High Price_macdhist'] = talib.MACD(
+        data['High Price'], fastperiod=12, slowperiod=26, signalperiod=9)
+    indicators['Low Price_macd'], indicators['Low Price_macdsignal'], indicators['Low Price_macdhist'] = talib.MACD(
+        data['Low Price'], fastperiod=12, slowperiod=26, signalperiod=9)
+    indicators['SAR'] = talib.SAR(data['High Price'], data['Low Price'])
+    indicators['HT_TRENDLINE'] = talib.HT_TRENDLINE(data['Close Price'])
+    indicators['HT_TRENDMODE'] = talib.HT_TRENDMODE(data['Close Price'])
+    indicators['HT_DCPERIOD'] = talib.HT_DCPERIOD(data['Close Price'])
+    indicators['HT_DCPHASE'] = talib.HT_DCPHASE(data['Close Price'])
+    indicators['TYPPRICE'] = talib.TYPPRICE(
+        data['High Price'], data['Low Price'], data['Close Price'])
+    return pd.DataFrame(indicators)
+
+
+def get_price_patterns(data):
+    """
+    Detects common price patterns using TA-lib, e.g. Two Crows, Belt-hold, Hanging Man etc.
+
+    :param data: DataFrame with data.
+    :return: DataFrame with pattern "likelihoods" on -200 - 200 scale.
+    """
+    patterns = {name: getattr(talib, name)(data['Open Price'], data['High Price'], data['Low Price'], data['Close Price'])
+                for name in talib.get_function_groups()['Pattern Recognition']}
+    return pd.DataFrame(patterns)
